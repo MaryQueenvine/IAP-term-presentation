1de954644c0b660675d814405d193bfa0ad373f8
MaryQueenvine <maryqueenvine.mwangi@strathmore.edu>
2024-11-22 11:40:45 +0300

    controller update

--- inventory_system/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ inventory_system/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Auth;
 
-
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
@@ -30,43 +29,29 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-        public
-        function store(Request $request)
-        {
-            $request->validate([
-                'email' => 'required|email',
-                'password' => 'required',
-                'two_factor_code' => 'nullable|string', // Add this for 2FA input
-            ]);
 
-            $credentials = $request->only('email', 'password');
+        // Custom 2FA logic
+        $request->validate([
+            'two_factor_code' => 'nullable|string', // Add this for 2FA input
+        ]);
 
-            if (Auth::attempt($credentials)) {
-                $user = Auth::user();
+        $user = Auth::user();
 
-                // Check if the user has 2FA enabled
-                if ($user->two_factor_enabled) {
-                    // If 2FA is enabled, verify the code
-                    $google2fa = new Google2FA();
-                    $valid = $google2fa->verifyKey($user->two_factor_secret, $request->input('two_factor_code'));
+        // Check if the user has 2FA enabled
+        if ($user->two_factor_enabled) {
+            $google2fa = new Google2FA();
+            $valid = $google2fa->verifyKey($user->two_factor_secret, $request->input('two_factor_code'));
 
-                    if (!$valid) {
-                        // Logout the user and show an error
-                        Auth::logout();
-                        return redirect()->back()->withErrors(['two_factor_code' => 'Invalid 2FA code']);
-                    }
-                }
-
-                // Redirect to intended page if login and 2FA are successful
-                return redirect()->intended('dashboard');
+            if (!$valid) {
+                // Logout the user and show an error
+                Auth::logout();
+                return redirect()->back()->withErrors(['two_factor_code' => 'Invalid 2FA code']);
             }
-
         }
+
+        // Redirect to intended page if login and 2FA are successful
         return redirect()->intended(RouteServiceProvider::HOME);
     }
-
-
-
 
     /**
      * Destroy an authenticated session.
